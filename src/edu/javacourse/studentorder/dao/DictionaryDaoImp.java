1977c5a09ab7d33fe4d08144315bf162504640c3
bobark <bobark@yandex.ru>
2023-01-12 13:29:29 +0700

    lesson 40 Connect Postgres DB, search Street

---
+++ src/edu/javacourse/studentorder/dao/DictionaryDaoImp.java
@@ -0,0 +1,43 @@
+package edu.javacourse.studentorder.dao;
+
+import edu.javacourse.studentorder.domain.Street;
+import edu.javacourse.studentorder.exception.DaoException;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DictionaryDaoImp implements DictionaryDao{
+
+    private static final String GET_STREET =  "SELECT street_code, street_name from jc_street  WHERE UPPER(street_name) LIKE UPPER(?)";
+
+    private Connection getConnection () throws SQLException {
+         Connection con = DriverManager.getConnection(
+                 "jdbc:postgresql://localhost:5432/jc_student",
+                 "postgres","17161815"
+         );
+         return con;
+     }
+
+    public List <Street> findStreet(String pattern) throws DaoException {
+        List<Street> result = new LinkedList<>();
+
+        try ( Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(GET_STREET)) {
+
+            stmt.setString(1,"%" + pattern + "%");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Street str = new Street(rs.getLong("street_code"), rs.getString("street_name"));
+                result.add(str);
+            }
+        }catch (SQLException ex){
+            throw new DaoException(ex);
+        }
+
+            return result;
+        }
+
+
+
+}
